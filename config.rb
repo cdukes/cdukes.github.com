687ed48716d1fdfb226edd937a474ef29ff042ce
Cooper Dukes <me@cooperdukes.com>
2013-05-04 17:56:28 +0800

    update to Inneo 3.0 styles

--- config.rb
+++ config.rb
@@ -3,8 +3,8 @@
 # Set this to the root of your project when deployed:
 http_path = "/"
 css_dir = "css"
-sass_dir = "scss"
-images_dir = "img"
+sass_dir = "sass"
+images_dir = "images"
 javascripts_dir = "js"
 
 output_style = :compressed
